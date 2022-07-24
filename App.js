566e43f1303762c2ff14aa4392df8222321703cc
Srajan <100074395+SrajanDongre@users.noreply.github.com>
2022-07-24 14:36:36 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import free from './free';
+import plus from './plus';
+import pro from './pro';
+
+const App=() =>{
+    return(
+        <div>
+            <free />
+            <plus />
+            <pro />
+        </div>
+    );
+};
+export default App;
